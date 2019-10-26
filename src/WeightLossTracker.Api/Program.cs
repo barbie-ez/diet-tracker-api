191f85efda51954c97d9664f622b097ccb7d5e24
barbie-ez <barbie.ezomo@gmail.com>
2019-10-26 12:21:34 +0100

    added docker support for easy deployment

--- src/WeightLossTracker.Api/Program.cs
+++ src/WeightLossTracker.Api/Program.cs
@@ -37,7 +37,7 @@ namespace WeightLossTracker.Api
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>()
-                .UseUrls("http://localhost:4000")
+                .UseUrls("http://*:1233")
                 ;
     }
 }
